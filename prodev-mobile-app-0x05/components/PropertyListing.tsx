d1c0390e29584069943e3b8ba8857ae6a9343ec7
Bethel Amadi <amadibethel@icloud.com>
2025-11-04 00:55:06 +0200

    Add updated files to project

--- prodev-mobile-app-0x05/components/PropertyListing.tsx
+++ prodev-mobile-app-0x05/components/PropertyListing.tsx
@@ -1,10 +1,16 @@
-import { View } from "react-native";
+import { View, Text } from "react-native";
 import PropertyListingCard from "./common/PropertyListingCard";
-import { PropertListing } from "../interfaces";
+import { useState } from "react";
+import { PropertListing, PropertyListingProps } from "../interfaces";
 
 const PropertyListing = ({ listings }: PropertListing) => {
   return (
-    <View style={{ flex: 1, rowGap: 10 }}>
+    <View
+      style={{
+        flex: 1,
+        rowGap: 10,
+      }}
+    >
       {listings?.map((listing, key) => (
         <PropertyListingCard {...listing} key={key} />
       ))}
@@ -13,4 +19,3 @@ const PropertyListing = ({ listings }: PropertListing) => {
 };
 
 export default PropertyListing;
-
